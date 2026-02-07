a495dc84744756a9f93fdad9e4dd0794b61e1480
TOPHE <johnchrostopher007@gmail.com>
2026-02-07 13:16:18 +0800

    Add files via upload

---
+++ MiniTask_#2/minitask2.java
@@ -0,0 +1,27 @@
+public class minitask2 {
+    public static void main(String[] args) {
+
+        // first group
+        for (int i = 1; i <= 4; i++) // count 1 to 4
+            {
+            System.out.print(i);
+        }
+
+        System.out.print(" 3 ");
+
+        // second group
+        for (int i = 5; i <= 7; i++) {
+            System.out.print(i);
+        }
+
+        System.out.print(" 6 ");
+
+        // third group
+        for (int i = 8; i <= 0 + 9; i++) {
+            System.out.print(i);
+        }
+        for (int i = 0; i <= 0; i++) {
+            System.out.print(i);
+        }
+    }
+}
